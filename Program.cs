5fcf421efb607b9479679c619d6e5db65163f2e5
James Grieve <jamesonrgrieve@gmail.com>
2020-08-13 12:03:07 -0600

    Two version of removing characters;

--- Program.cs
+++ Program.cs
@@ -14,15 +14,40 @@ namespace LetterRemover
             Last Modified: August 13, 2020
             */
 
-            string userInput, userChar, output;
+            string userInput, userChar, output="";
             
             Console.WriteLine("Welcome to the letter remover application! You will be prompted to enter both a string and a character, and every instance of that character will be removed from the string.\n");
 
             Console.Write("Please enter a string from which characters will be removed: ");
-            userInput = Console.ReadLine();
+            //userInput = Console.ReadLine();
+            userInput = "Jaames";
 
             Console.Write("Please enter a character to remove from the string: ");
-            userChar = Console.ReadLine();
+            //userChar = Console.ReadLine();
+            userChar = "a";
+            
+            /*
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                if (userInput[i] != userChar[0])
+                {
+                    output += userInput[i];
+                }
+            }
+            */
+        
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                if (userInput[i] == userChar[0])
+                {
+                    userInput = userInput.Remove(i, 1);
+                    i--;
+                }
+            }
+            output = userInput;
+
+
+            Console.WriteLine($"Your modified string is on the next line:\n{output}");
         }
     }
 }
