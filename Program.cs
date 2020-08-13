cfdd6a9bae70fbe44cb818444a61bcdb7da17ec1
James Grieve <jamesonrgrieve@gmail.com>
2020-08-13 11:22:45 -0600

    Added prompts for string and character;

--- Program.cs
+++ Program.cs
@@ -18,7 +18,11 @@ namespace LetterRemover
             
             Console.WriteLine("Welcome to the letter remover application! You will be prompted to enter both a string and a character, and every instance of that character will be removed from the string.\n");
 
-            
+            Console.Write("Please enter a string from which characters will be removed: ");
+            userInput = Console.ReadLine();
+
+            Console.Write("Please enter a character to remove from the string: ");
+            userChar = Console.ReadLine();
         }
     }
 }
