f8ed672a836ec245f967dcb5c1d5bd0482e3c86d
James Grieve <jamesonrgrieve@gmail.com>
2020-08-13 11:07:58 -0600

    Initial commit; Added README.md;

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LetterRemover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Letter Remover
+Practice from August 12, 2020 for Cohort 4.2.
